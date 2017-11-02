727054541c16f836fea22e7ca6a7e1d3c569e5af
roger <chen.rojer@gmail.com>
2017-11-02 23:16:31 +0800

    init

---
+++ src/main/java/com/roger/App.java
@@ -0,0 +1,16 @@
+
+
+package com.roger;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+
+    }
+}
